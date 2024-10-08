1a5f91df163a810ea44c1231ea547efe0e3af14f
Dat Nguyen <kimdat546@gmail.com>
2024-10-08 22:59:24 +0700

    Initial Commit

---
+++ RainyReserve/RainyReserveApp.swift
@@ -0,0 +1,17 @@
+//
+//  RainyReserveApp.swift
+//  RainyReserve
+//
+//  Created by Nguyễn Đạt on 08/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct RainyReserveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
